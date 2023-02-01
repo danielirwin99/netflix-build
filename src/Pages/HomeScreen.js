d8e059f28fe22ee3426717ed8934d17b5322518b
Daniel <danielirwin99@gmail.com>
2023-02-01 11:11:12 +1100

    Initial Setup

---
+++ src/Pages/HomeScreen.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Nav from "../Components/Nav";
+import "../Styles/HomeScreen.css";
+
+const HomeScreen = () => {
+  return (
+    <div className="homeScreen">
+      {/* Nav */}
+      <Nav />
+
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+      <h1>Test</h1>
+
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  );
+};
+
+export default HomeScreen;
---
+++ src/Utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDyn_XLy49XM02tE5FyfTpZtgaPjCy44qc",
+  authDomain: "netflix-build-2a9d2.firebaseapp.com",
+  projectId: "netflix-build-2a9d2",
+  storageBucket: "netflix-build-2a9d2.appspot.com",
+  messagingSenderId: "659306224376",
+  appId: "1:659306224376:web:55e936862846ac559d790d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
