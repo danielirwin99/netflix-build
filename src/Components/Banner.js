4e921354fd0baf62b869ccd7670868a8f4fcd082
danielirwin99 <danielirwin99@gmail.com>
2023-02-01 15:24:13 +1100

    Fetching Api through Axios

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -2,10 +2,9 @@ import React from "react";
 import "../Styles/Banner.css";
 
 const Banner = () => {
-
-    function truncate(string, n) {
-        return string?.length > n ? string.substr(0, n - 1) + "..."
-    }
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  }
 
   return (
     <header
@@ -22,7 +21,12 @@ const Banner = () => {
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
         </div>
-        <h1 className="banner__description">This is a test description</h1>
+        <h1 className="banner__description">
+          {truncate(
+            `This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description This is a test description`,
+            150
+          )}
+        </h1>
       </div>
       <div className="banner--fadeBottom" />
     </header>
